2757481e670a203b8585d7105de3d24cc3aebab8
Jacob D. Martin <91269723+aNixxom@users.noreply.github.com>
2022-04-26 09:23:25 -0400

    Created script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let typed = new Typed('#typed',{
+    strings: ['<strong>Welcome</strong>', 'jaocb@jacobm.dev', 'Hello, world!'],
+    typeSpeed: 1,
+    backSpeed: 40,
+    typeSpeed: 40,
+    startDelay: 10,
+    loop: true,
+  });
