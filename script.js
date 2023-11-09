27fff46b0764f1135d26e52692289549725bb569
aNixxom <jacob@jacobm.dev>
2023-11-09 09:03:13 -0500

    Update

--- script.js
+++
@@ -1,8 +0,0 @@
-let typed = new Typed('#typed',{
-    strings: ['<strong>Welcome</strong>', 'jaocb@jacobm.dev', 'Hello, world!'],
-    typeSpeed: 1,
-    backSpeed: 40,
-    typeSpeed: 40,
-    startDelay: 10,
-    loop: true,
-  });
